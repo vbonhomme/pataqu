147844a345d90ba9ab402bac6a43651020d6e1c1
vbonhomme <bonhomme.vincent@gmail.com>
2022-04-19 14:44:52 +0200

    +bin

--- R/bin.R
+++ R/bin.R
@@ -3,14 +3,25 @@
 #' Bin and summarise without [fitting]
 #'
 #' @param df [tibble()] typically returned by [quake()]
-#' @param y colname of the value of interest. Default to `y`.
-#' @param fun to use to summarise `y`
+#' @param y,x colnames to use. Default to `y`/`x_new`
 #' @param by colname for grouping structure (besides `k`). Default to `NULL`
-#' @param x_pred the sequence of x for which to predict new values after fitting
+#' @param fun to use to summarise `y`
 #' @param k colname for iteration . Default to `k`.
 #'
-bin <- function(df, y=y, fun=stats::median, by=NULL,
-                      x_pred=cutter_number(df, 30, x_new),
+#' @examples
+#' animals_q %>% bin(y=value, by=taxa, fun=mean)
+#'
+#' animals_q %>% bin(y=value, fun=mean)
+#' @export
+bin <- function(df, y=y, x=x_new, by=NULL, fun=stats::median,
+                      # x_bin,
                       k=k){
+  # todo implement
+  # if (missing(x_pred))
+  #   df %>% dplyr::pull({{x}}) %>% ggplot2::cut_number(5)
 
+  df %>%
+    dplyr::mutate(x_bin=ggplot2::cut_number({{x}}, 5)) %>%
+    dplyr::group_by({{k}}, {{by}}, x_bin) %>%
+    dplyr::summarise(y_bin=fun({{y}}), .groups = "drop")
 }
