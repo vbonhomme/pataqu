077cbbb829fa313e3613254be5580844eb4c0bb3
vbonhomme <bonhomme.vincent@gmail.com>
2022-04-19 12:45:37 +0200

    rebegin test

--- tests/testthat/test-shake.R
+++ tests/testthat/test-shake.R
@@ -1,10 +1,7 @@
-#
-# test_that("hello prints", {
-#   x <- tibble::tibble(tpq=1:10, taq=tpq+1, y=10:1) %>% shake_uniform()
-#   expect_s3_class(x, "tbl_df")
-#   expect_true(all((x$x_new>=x$tpq) & ((x$x_new<=x$taq))))
-#
-#   x <- tibble::tibble(mean=0, sd=1:10, y=10:1) %>% shake_gaussian()
-#   expect_s3_class(x, "tbl_df")
-# })
-#
+
+test_that("shake works fine", {
+  x <- df_u %>% shake_uniform(tpq , taq)
+  expect_s3_class(x, "tbl_df")
+
+})
+
